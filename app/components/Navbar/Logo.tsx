cd5bba5b1b06eafb00538a9c603adfe80391967e
Giancarlos Isasi <giancarlos.isasi@gmail.com>
2023-04-30 13:24:14 -0500

    feat: reset url when click on logo

--- app/components/Navbar/Logo.tsx
+++ app/components/Navbar/Logo.tsx
@@ -8,7 +8,9 @@ type TLogoProps = {}
 const Logo: React.FC<TLogoProps> = () => {
   const router = useRouter()
 
-  return <Image alt='logo' className='hidden md:block cursor-pointer'
+  return <Image onClick={()=> {
+    router.push('/')
+  }} alt='logo' className='hidden md:block cursor-pointer'
     height="100" width="100" src="/images/logo.png" />
 }
 
